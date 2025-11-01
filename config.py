b7c3a38705c6366c8b4ee5b621b422ea8be89682
Gentlesprite32 <shiqisun217@gmail.com>
2025-11-01 20:49:08 +0800

    Update config.py

--- config.py
+++ config.py
@@ -3,6 +3,7 @@
 # Software:PyCharm
 # Time:2025/9/9 14:19
 # File:config.py
-activity_id = '828401'
-flow_id = '1159998'
+activity_id = '836022'
+flow_id = '1166471'
 sd_id = '8820a53e535dd3ce12edfeac13288eec'
+
