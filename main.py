117cb16c5f8905afa0abfff261c0e486617a1a81
Gentlesprite32 <shiqisun217@gmail.com>
2026-03-01 17:04:36 +0800

    Update main.py

--- main.py
+++ main.py
@@ -7,21 +7,30 @@ import os
 import sys
 
 from module import console
-from module.stdio import get_cookies, get_push_key
+from module.parser import PARSE_ARGS
 from module.signer import NZSigner
 from module.handler import Handler
-from config import activity_id, flow_id, sd_id
+
+from module.stdio import (
+    get_cookies,
+    get_push_key
+)
+from config import (
+    activity_id,
+    flow_id,
+    sd_id,
+    special_date,
+    special_date_flow_id
+)
 
 if __name__ == '__main__':
     cookies = get_cookies()
     push_key = get_push_key()
-    actions_flags = False
     if not cookies:
         cookies = os.getenv('COOKIES')
         push_key = os.getenv('PUSH_KEY')
         if not cookies:
-            actions_flags = True
-            console.print(
+            console.log(
                 '请配置以下环境变量或新建config.py设置以下变量后运行。\n'
                 '# Linux/macOS 设置教程:export 变量="your_cookie_value_here"\n'
                 '# Windows 设置教程:set 变量=your_cookie_value_here\n'
@@ -35,9 +44,11 @@ if __name__ == '__main__':
         signer.sign(
             activity_id=activity_id,
             flow_id=flow_id,
-            sd_id=sd_id
+            sd_id=sd_id,
+            special_date=special_date,
+            special_date_flow_id=special_date_flow_id
         )
-        if not actions_flags:
+        if not PARSE_ARGS.loop:
             sys.exit()
         handler = Handler()
         handler.task(
