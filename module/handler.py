15d0c25812ff5f39077fca3a2470806ac126a551
Gentlesprite32 <shiqisun217@gmail.com>
2026-03-01 17:03:04 +0800

    Update handler.py

--- module/handler.py
+++ module/handler.py
@@ -23,21 +23,26 @@ class Handler:
     @staticmethod
     def task(func: Callable, **kwargs):
         schedule_tables = [
-            '00:00'
+            '00:00:00'
         ]
         today = datetime.datetime.now().date()
         remain_do_time = []
         scheduler = sched.scheduler(time.time, time.sleep)
         for time_str in schedule_tables:
-            scheduled_time = datetime.datetime.strptime(f'{today} {time_str}', '%Y-%m-%d %H:%M')
+            # 根据时间字符串长度判断格式，兼容 HH:MM 和 HH:MM:SS。
+            if len(time_str.split(':')) == 2:
+                time_format = '%Y-%m-%d %H:%M'
+            else:
+                time_format = '%Y-%m-%d %H:%M:%S'
+            scheduled_time = datetime.datetime.strptime(f'{today} {time_str}', time_format)
             if scheduled_time < datetime.datetime.now():
                 scheduled_time += datetime.timedelta(days=1)
             delay = (scheduled_time - datetime.datetime.now()).total_seconds()
             remain_do_time.append(delay)
         next_do_time = min(remain_do_time)
         scheduler.enter(next_do_time, 1, func, kwargs=kwargs)
-        p1 = f'开始执行任务,当前时间:{datetime.datetime.now()}'
-        p2 = f'距离下次执行任务还有%d:%02d:%02d' % (Handler.__to_hour_minute(next_do_time))
+        p1 = f'开始执行任务,当前时间:{datetime.datetime.now()}。'
+        p2 = f'距离下次执行任务还有%d:%02d:%02d。' % (Handler.__to_hour_minute(next_do_time))
         log.info(p1)
         log.info(p2)
         console.log(p1)
