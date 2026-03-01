98b51f637506d3d3e324bb55b672ff170404f60b
Gentlesprite32 <shiqisun217@gmail.com>
2026-03-01 17:04:22 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,10 @@
+# coding=UTF-8
+# Author:Gentlesprite
 # Software:PyCharm
 # Time:2025/9/9 14:19
 # File:config.py
-activity_id = '880706'
-flow_id = '1192657'
-sd_id = '8820a53e535dd3ce12edfeac13288eec'
+activity_id: str = '880706'
+flow_id: str = '1192657'
+sd_id: str = '8820a53e535dd3ce12edfeac13288eec'
+special_date: list = ['2026-03-03', '2026-03-13', '2026-03-20', '2026-03-27']
+special_date_flow_id: str = '1192661'
