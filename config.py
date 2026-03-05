ba4547e6e0e9360a3369dde79617738766fc86e6
Gentlesprite32 <shiqisun217@gmail.com>
2026-03-05 19:44:03 +0800

    Update config.py

--- config.py
+++ config.py
@@ -3,9 +3,11 @@
 # Software:PyCharm
 # Time:2025/9/9 14:19
 # File:config.py
+import random
+
 activity_id: str = '880706'
 flow_id: str = '1192657'
-sd_id: str = '8820a53e535dd3ce12edfeac13288eec'
+sd_id: str = ''.join(random.choices('0123456789abcdef', k=32))
 special_date: list = ['2026-03-03', '2026-03-13', '2026-03-20', '2026-03-27']
 special_date_flow_id: str = '1192661'
 cumulative_day: list = [3, 5, 9, 15, 20, 25]
