d0f9b84fc789f970b680c0f358249b6a553108e7
Gentlesprite32 <shiqisun217@gmail.com>
2026-04-02 14:45:48 +0800

    Update config.py

--- config.py
+++ config.py
@@ -5,10 +5,10 @@
 # File:config.py
 import random
 
-activity_id: str = '880706'
-flow_id: str = '1192657'
+activity_id: str = '885966'
+flow_id: str = '1197212'
 sd_id: str = ''.join(random.choices('0123456789abcdef', k=32))
-special_date: list = ['2026-03-03', '2026-03-13', '2026-03-20', '2026-03-27']
-special_date_flow_id: str = '1192661'
+special_date: list = ['2026-04-01', '2026-04-10', '2026-04-17', '2026-04-24']
+special_date_flow_id: str = '1197216'
 cumulative_day: list = [3, 5, 9, 15, 20, 25]
-cumulative_day_flow_id: str = '1192656'
+cumulative_day_flow_id: str = '1197211'
