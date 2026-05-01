e542fefc82afc52d6d7ceb51004a900af017021a
Gentlesprite32 <shiqisun217@gmail.com>
2026-05-01 12:20:59 +0800

    Update config.py

--- config.py
+++ config.py
@@ -6,13 +6,13 @@
 import random
 from typing import Optional
 
-activity_id: str = '885966'
-flow_id: str = '1197212'
+activity_id: str = '920173'
+flow_id: str = '1206535'
 sd_id: str = ''.join(random.choices('0123456789abcdef', k=32))
-special_date: list = ['2026-04-01', '2026-04-10', '2026-04-17', '2026-04-24']
-special_date_flow_id: str = '1197216'
+special_date: list = ['2026-05-08', '2026-05-15', '2026-05-22', '2026-05-30']
+special_date_flow_id: str = '1206539'
 cumulative_day: list = [3, 5, 9, 15, 20, 25]
-cumulative_day_flow_id: str = '1197211'
+cumulative_day_flow_id: str = '1206534'
 
 # 版本福利配置（可选）。
 version_gift_activity_id: Optional[str] = '916364'
