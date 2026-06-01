e373c49597fdd7d2f1269c3077be964d6e8d3c80
Gentlesprite32 <shiqisun217@gmail.com>
2026-06-01 20:00:11 +0800

    Update config.py

--- config.py
+++ config.py
@@ -6,16 +6,16 @@
 import random
 from typing import Optional
 
-activity_id: str = '925755'
-flow_id: str = '1212133'
+activity_id: str = '925755'  # 签到活动ID。
+flow_id: str = '1212133'  # 签到礼包。
 sd_id: str = ''.join(random.choices('0123456789abcdef', k=32))
-special_date: list = ['2026-05-08', '2026-05-15', '2026-05-22', '2026-05-30']
-special_date_flow_id: str = '1212132'
+special_date: list = ['2026-06-05', '2026-06-12', '2026-06-19', '2026-06-26']
+special_date_flow_id: str = str(int(flow_id) + 5)  # 限定日期礼包。
 cumulative_day: list = [3, 5, 9, 15, 20, 25]
-cumulative_day_flow_id: str = '1212138'
+cumulative_day_flow_id: str = str(int(flow_id) - 1)  # 累计签到礼包。
 
 # 版本福利配置（可选）。
-version_gift_activity_id: Optional[str] = '924173'
-version_gift_play_flow_id: Optional[str] = '1209929'
-version_gift_share_flow_id: Optional[str] = '1209930'
-version_gift_flow_id: Optional[str] = '1209931'
+version_gift_activity_id: Optional[str] = '924173'  # 版本福利活动ID。
+version_gift_play_flow_id: Optional[str] = '1209929'  # 游玩礼包。
+version_gift_share_flow_id: Optional[str] = str(int(version_gift_play_flow_id) + 1)  # 分享礼包。
+version_gift_flow_id: Optional[str] = str(int(version_gift_play_flow_id) + 2)  # 抽奖。
