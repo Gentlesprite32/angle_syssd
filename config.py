651516cd95d4f1b1f04db6f08d420175b85bdaf1
Gentlesprite32 <shiqisun217@gmail.com>
2026-08-03 21:23:51 +0800

    Update config.py

--- config.py
+++ config.py
@@ -15,8 +15,8 @@ cumulative_day: list = [3, 5, 9, 15, 20, 25]
 cumulative_day_flow_id: str = str(int(flow_id) - 1)  # 累计签到礼包。
 
 # 版本福利配置（可选）。
-version_gift_activity_id: Optional[str] = '947081'  # 版本福利活动ID。
-version_gift_play_flow_id: Optional[str] = '1226032'  # 游玩一局礼包。
+version_gift_activity_id: Optional[str] = None  # 版本福利活动ID。
+version_gift_play_flow_id: Optional[str] = None  # 游玩一局礼包。
 version_gift_share_flow_id: str = str(
     int(version_gift_play_flow_id) + 1) if version_gift_play_flow_id else None  # 分享礼包。
 version_gift_flow_id: str = str(int(version_gift_play_flow_id) + 2) if version_gift_play_flow_id else None  # 抽奖。
